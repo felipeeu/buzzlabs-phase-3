48d72448e1b535c48bffbca44228770de392637d
Felipe Domingues <felipeeu@yahoo.com.br>
2025-06-14 16:54:45 -0300

    Connect with local datomic db and  use datomic samples databases to test

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.datomic/local "1.0.291"]
+                 [com.datomic/client "1.0.139"]
+                 ]
   :main ^:skip-aot buzzlabs-phase-3.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/buzzlabs_phase_3/core.clj
+++ src/buzzlabs_phase_3/core.clj
@@ -1,7 +1,12 @@
 (ns buzzlabs-phase-3.core
+  (:require [datomic.client.api :as d])
   (:gen-class))
+
+(def client (d/client {:server-type :datomic-local
+                       :system "datomic-samples"}))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (println (d/list-databases client {}))
   (println "Hello, World!"))
